a229dbc2381c725978e3770f6026350662c31101
sajedeh <132915475+SajedehF@users.noreply.github.com>
2023-12-11 20:59:14 +0330

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: { },
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'1024px',
+      xl:'1440px'
+      
+    },
+    fontFamily:{
+     'poppins': ['Poppins', 'sans-serif'],
+                                                
+    }
+    
+    
+  },
+  plugins: [],
+}
+
